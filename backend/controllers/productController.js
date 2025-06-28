935bcbd7d900037b5e9466a7e10d89a19a82b64b
pranaykumar98 <manepallypranay98@gmail.com>
2025-06-28 16:25:56 +0530

    pricing changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,36 +6,6 @@ exports.getAllProducts = (req, res) => {
   res.json(products);
 };
 
-// exports.purchaseProduct = (req, res) => {
-//   const { productId, quantity, discount = 0 } = req.body;
-//   console.log("checking", req.body);
-
-//   const product = products.find((p) => p.id === productId);
-//   if (!product) return res.status(404).json({ error: "Product not found" });
-//   if (quantity > product.stock)
-//     return res.status(400).json({ error: "Insufficient stock" });
-
-//   const markedUpPrice = product.price * 1.2;
-//   const discountedPrice = markedUpPrice * (1 - discount / 100);
-//   const finalPrice = discountedPrice * quantity;
-
-//   product.stock -= quantity;
-
-//   res.json({
-//     finalPrice: finalPrice.toFixed(2),
-//     updatedStock: product.stock,
-//   });
-// };
-
-// exports.getCalculations = (req, res) => {
-//   const { price, quantity, discount = 0 } = req.body;
-//   const markedUpPrice = price * 1.2;
-//   const discountedPrice = markedUpPrice * (1 - discount / 100);
-//   const finalPrice = discountedPrice * quantity;
-//   console.log("checkingforfinalprice", finalPrice);
-//   res.json({ finalPrice: finalPrice });
-// };
-
 exports.purchaseProduct = (req, res) => {
   const { productId, quantity, discount = 0 } = req.body;
   console.log("checking", req.body);
